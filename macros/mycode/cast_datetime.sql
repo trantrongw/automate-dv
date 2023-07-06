2f170dcf183802c4de4d3f56c36827ed1b02e5c7
NghiaTT <trantrongnghia1991@gmail.com>
2023-07-06 15:43:21 +0700

    fix cast datetime

--- macros/mycode/cast_datetime.sql
+++
@@ -1,7 +0,0 @@
-{%- macro default_cast_datetime(column_str, as_string=false, alias=none, date_type=none) -%}
-
-    CONVERT(DATETIME2(6), {{ column_str }})
-
-    {%- if alias %} AS {{ alias }} {%- endif %}
-
-{%- endmacro -%}
--- macros/supporting/casting/cast_datetime.sql
+++ macros/supporting/casting/cast_datetime.sql
@@ -33,7 +33,7 @@
 
 {%- macro fabric__cast_datetime(column_str, as_string=false, alias=none, date_type=none) -%}
 
-    CONVERT(DATETIME2, {{ column_str }})
+    CONVERT(DATETIME2(6), {{ column_str }})
 
     {%- if alias %} AS {{ alias }} {%- endif %}
 
